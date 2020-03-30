9bc67262f86794e70cb596fc12a21aa0db6fb6a2
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2020-03-31 02:58:31 +0530

    Updated config file

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
-# python ./main.py "train_4.yml"
-
-python ./main.py "test_4.yml"
+python ./main.py "train_a_1.yml"
+python ./main.py "train_a_2.yml"
+python ./main.py "train_a_3.yml"
+python ./main.py "train_a_4.yml"
+python ./main.py "train_a_5.yml"
