99140bbaef390a87c2196093d9526f7039166347
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2020-04-06 17:10:46 +0530

    Producer consumer telegram support added

--- utils/telegram_update.py
+++ utils/telegram_update.py
@@ -7,17 +7,14 @@ from utils.print_util import cprint
 
 def publish(results):
     ctx = zmq.Context()
-    sock = ctx.socket(zmq.PUB)
-    sock.bind("tcp://192.168.100.2:1234")
+    sock = ctx.socket(zmq.PUSH)
+    sock.connect("tcp://192.168.100.21:1234")
 
     cprint("[ Publishing results on Telegram ]", type="info3")
 
     time.sleep(1)
-    sock.send_multipart(
-        [
-            bytes("experiment", 'utf-8'),
-            bytes(json.dumps(results), 'utf-8')
-        ]
+    sock.send(
+        bytes(json.dumps(results), 'utf-8')
     )
 
     sock.close()
