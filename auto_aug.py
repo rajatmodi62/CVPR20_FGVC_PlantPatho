00fa68ed189774c73fc8a6a239748367a5ce4f39
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2020-04-17 21:21:56 +0530

    Prettier print

--- auto_aug.py
+++ auto_aug.py
@@ -1,6 +1,7 @@
 from auto_aug_utils.utils import (sample_policy)
 from auto_aug_utils.writer import (Writer)
 from train import train
+from utils.print_util import cprint
 
 TOP_POLICY_CNT = 5
 SUB_POLICY_CNT = 5
@@ -11,20 +12,23 @@ AUG_SEARCH_LOOP = 3
 def search(config, device):
     writer = Writer()
 
-    print("getting baseline policy")
+    cprint("[ Getting baseline policy ]", type="info2")
     # get a baseline score with no augmentations
     best_loss, best_roc = train(config, device, policy=None)
     writer.write(-1, best_roc)
+    cprint("[ Baseline score: " + str(best_roc) + " ]", type="success")
 
     policies = []
 
     # augmentation search loop
     for i in range(AUG_SEARCH_LOOP):
+        cprint("[ Policy search Iteration : " + str(i + 1) +
+               "/" + str(AUG_SEARCH_LOOP) + " ]", type="info2")
+
         # sample a policy from pool
         policy = sample_policy(SUB_POLICY_CNT, OPERATIONS_PER_SUB)
 
-        print("sampling policy",i)
-	# get best score using the policy
+        # get best score using the policy
         best_loss, best_roc = train(config, device, policy=policy)
         writer.write(i, best_roc)
 
@@ -37,5 +41,7 @@ def search(config, device):
 
         # save policy list
         writer.freeze_policies(policies)
+
+        cprint("[ Policy Score: " + str(best_roc) + " ]", type="success")
 
     pass
